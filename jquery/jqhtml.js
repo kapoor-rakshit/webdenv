5f7657de32a37e1a8e5ba15fd7ccebe8e976cccd
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2019-10-08 23:09:36 +0530

    Update jqhtml.js

--- jquery/jqhtml.js
+++ jquery/jqhtml.js
@@ -4,83 +4,91 @@ $(document).ready(function(){
 	$("button").click(function(){
 
 		alert($("#chk").text());                    // get text only, ignoring all tags (if written within selected elem).
-
 		$("#disp").text("<b>hello</b>");           // set text, tags are considered as a text too.
 
+		
 		alert($("#chk").html());                  // get html code, with tags (if written within selected elem).
-
 		$("#disp").html("<b>Hello in bold</b>");  // set html code
 
-	    $("#test3").val();                       // get value of form elems
+		
+	        $("#test3").val();                       // get value of form elems
+	        $("#test3").val("Value set");           // set value of form elems
 
-	    $("#test3").val("Value set");           // set value of form elems
-
-	    $("#chk").attr("href");                // get value of attribute
-
-	    $("#w3s").attr({
-        "href" : "https://www.w3schools.com/jquery/",  // set value of attribute (here multiple attribs are dealt with)
-        "title" : "W3Schools jQuery Tutorial"
-					    });
-
-	    $("form :input").removeAttr("title");        // remove Attribute
-
-	    var x = $("p").position();                   // returns position relative to the offset parent of top and left
-	    alert("Top: " + x.top + " Left: " + x.left); // NOTE : positon value cannot be set
-
-	    var x = $("p").offset();                    // returns position relative to the document of top and left
-	    alert("Top: " + x.top + " Left: " + x.left);
-
-	    $("p").offset({top:value,left:value});     // set offset values
+		
+	        $("#chk").attr("href");                // get value of attribute
+		$("#w3s").attr({
+                    "href" : "https://www.w3schools.com/jquery/",  // set value of attribute (here multiple attribs are dealt with)
+                    "title" : "W3Schools jQuery Tutorial"});
+		$("#w3s").attr("href", function(i, origValue)      // Callback Function for attr() , the index of current element in list of elements selected and original (old) attribute value.
+                     { return origValue + "/jquery/"; });
 
 
-// add new elements/content.
+	         var x = $("p").position();                   // returns position relative to the offset parent of top and left
+	         alert("Top: " + x.top + " Left: " + x.left); // NOTE : positon value cannot be set
 
-	    $("ol").append("<li>Appended item</li>");     // append an elem/text, making it a child of selected elem
-
-	    $("ol").prepend("<li>Prepended item</li>");   // prepend an elem/text, making it a child of selected elem
-
-	    $("<li>Appended item</li>").appendTo("ol");
-
-		$("<li>Prepended item</li>").prependTo("ol");	    
-
-	    $("img").after("Some text after img");           // insert elem/text after selected elem
-
-		$("img").before("Some text before img");        // insert elem/text before selected elem
-
-		$("Some text after img").insertAfter("img");
-
-		$("Some text before img").insertBefore("img");
+		
+	         var x = $("p").offset();                    // returns position relative to the document of top and left
+	         alert("Top: " + x.top + " Left: " + x.left);
+	         $("p").offset({top:value,left:value});     // set offset values
 
 
-// remove existing HTML elements.
+	         $("ol").append("<li>Appended item</li>");     // inserts content AT THE END of the selected HTML elements , making it a child of selected elem
+		 $("<li>Appended item</li>").appendTo("ol");
+		
+		
+	         $("ol").prepend("<li>Prepended item</li>");   // inserts content AT THE BEGINNING of the selected HTML elements , making it a child of selected elem
+		 $("<li>Prepended item</li>").prependTo("ol");	    
 
-		$("#div1").detach();                      // removes matched elem, but retains JQuery data and events associated with removed elem.
+	         
+		 var txt1 = "<p>Text.</p>";               // Create element with HTML
+		 var txt2 = $("<p></p>").text("Text.");   // Create with jQuery
+		 var txt3 = document.createElement("p");  // Create with DOM
+		 txt3.innerHTML = "Text.";
+		 $("body").append(txt1, txt2, txt3);      // Append the new elements
+		
+		
+	         $("img").after("Some text after img");           // insert elem/text after selected elem
+                 $("<i>After</i>").insertAfter("img");
+		
+		
+		 $("img").before("Some text before img");        // insert elem/text before selected elem
+		 $("Some text before img").insertBefore("img");
+		
+		
+		 var txt1 = "<b>I </b>";                    // Create element with HTML
+		 var txt2 = $("<i></i>").text("love ");     // Create with jQuery
+		 var txt3 = document.createElement("b");    // Create with DOM
+		 txt3.innerHTML = "jQuery!";
+		 $("img").after(txt1, txt2, txt3);          // Insert new elements after <img>
 
-		$("#div1").detach("img");                 
 
-		$("#div1").remove();                      // removes the selected element(s) and its child elements, along with associated events
+		 $("#div1").detach();                      // removes matched elem, but retains JQuery data and events associated with removed elem.
+		 var x;
+		    $("#btn1").click(function(){
+			 x = $("p").detach(); });          // keeps all events of detached element 
+		    $("#btn2").click(function(){
+			    $("body").prepend(x); });
+		
+		
+		 $("#div1").remove();                      // removes the selected element(s) and its child elements, along with associated events
+		 $("p").remove(".test, .demo");           // filter applied (removes all <p> elements with class="test" or class="demo")
+		
+		
+		 $("#div1").empty();                       // removes the child elements of the selected element(s)
+		
+		
+		 $("form :input").removeAttr("title");    // remove Attribute
+		 $("p, #tag1").removeAttr("id class");    // Remove several attributes from the selected elements
 
-		$("#div1").empty();                       // removes the child elements of the selected element(s), same as .remove()
 
-		$("p").remove(".test, .demo");           // filter applied (removes all <p> elements with class="test" and class="demo")
-
-		$("form :input").removeAttr("title");    // remove Attribute
-
-
- // replace elements
-
-		$("<b>New Content</b>").replaceAll("p");   // Replace occurrence of "p" tag and it's content with "<b>New Content</b>"
-		$(".newclass").replaceAll(".oldclass");
-
-		$(".oldclass").replaceWith(".newclass");
-
-
- // clone
-
-		$(".classnaam").clone().appendTo("p");   // clone(withDataAndEvents, deepWithDataAndEvents)
-												// withDataAndEvents : data and events handlers along with elements. default is false
-					 // deepWithDataAndEvents : data and events handlers along with elements for all child elements. default is false
-
+		 $("<b>New Content</b>").replaceAll("p");   // Replace occurrence of "p" tag and it's content with "<b>New Content</b>"
+		 $(".newclass").replaceAll(".oldclass");
+		 $(".oldclass").replaceWith(".newclass");
+		 
+		
+		 $(".classnaam").clone().appendTo("p");   // clone(withDataAndEvents, deepWithDataAndEvents)
+		                                          // withDataAndEvents : events handlers should be copied. default is false
+		                                          // deepWithDataAndEvents : events handlers should be copied for all child elements. default is false
 
 	});
 });
