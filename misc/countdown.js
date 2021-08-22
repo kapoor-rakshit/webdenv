e78f2f98273a8575800bb3a0dd6c027bae2350e3
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2021-08-22 12:41:09 +0530

    Update countdown.js

--- misc/countdown.js
+++ misc/countdown.js
@@ -1,24 +1,24 @@
 // Reference : https://www.w3schools.com/howto/howto_js_countdown.asp
 
-																	                                                  // Set the date we're counting down to
+		                                                                     // Set the date we're counting down to
 var countDownDate = new Date("june 28, 2018 16:30:00").getTime();
 
-																	                                                  // Update the count down after every 1 second
+										     // Update the count down after every 1 second
 var x = setInterval(function() {
 
-																                                                    // Get todays date and time
+										     // Get todays date and time
     var now = new Date().getTime();
     
-																                                                    // Find the distance between now an the count down date
+										     // Find the distance between now an the count down date
     var distance = countDownDate - now;
     
-																                                                    // Time calculations for days, hours, minutes and seconds
+										     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-																                                                    // Output the result in an element with id="demo"
+										    // Output the result in an element with id="demo"
     document.getElementById("content").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
     
