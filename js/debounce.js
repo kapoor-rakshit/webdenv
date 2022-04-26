b94c892d342d0f468b474b241825b2ecc358059e
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2022-04-26 15:25:27 +0530

    Update debounce.js

--- js/debounce.js
+++ js/debounce.js
@@ -10,13 +10,12 @@ REFERENCE: https://www.geeksforgeeks.org/debouncing-in-javascript/
 */
 
 const debounce = (func, delay) => {
-    let debounceTimer
+    var debounceTimer;
     return function() {
-        const context = this
-        const args = arguments
-            clearTimeout(debounceTimer)
-                debounceTimer
-            = setTimeout(() => func.apply(context, args), delay)
+        const context = this;
+        const args = arguments;
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => func.apply(context, args), delay);
     }
 }
 
